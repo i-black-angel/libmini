1ef737be03cc6660647386fa9a3d56bc8aaf33bc
i-black-angel <iblackangel@163.com>
2017-11-21 13:39:24 +0800

    update autogen-clean.sh

--- autogen-clean.sh
+++ autogen-clean.sh
@@ -20,5 +20,6 @@ if [ -f Makefile ]; then
 fi
 echo "Removing autogenned files..."
 rm -f config.guess config.sub configure install-sh missing mkinstalldirs Makefile.in ltmain.sh stamp-h.in */Makefile.in ltconfig stamp-h config.h.in config.h.in~ aclocal.m4 ar-lib compile depcomp
-rm -rf m4 autom4te.cache
+rm -f m4/libtool.m4 m4/lt~obsolete.m4 m4/ltoptions.m4 m4/ltsugar.m4 m4/ltversion.m4
+rm -rf autom4te.cache
 echo "Done."
